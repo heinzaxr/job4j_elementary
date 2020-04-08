78ec029481ecd933e3df1f9f8dd450b9488f8c7a
arkanomsk <49816097+arkanomsk@users.noreply.github.com>
2020-04-08 10:12:10 +0600

    3. Арифметические операции.[#269222]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexandr Geynts");
+        System.out.println("08.03.1985");
+    }
+}
