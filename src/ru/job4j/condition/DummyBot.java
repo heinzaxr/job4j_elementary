95a8be1e92a0e7a8b6c07d12e46e16fc028b955b
arkanomsk <49816097+arkanomsk@users.noreply.github.com>
2020-04-11 23:47:25 +0600

    4.1.2. Глупый бот.[#269171]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -1,4 +1,17 @@
 package ru.job4j.condition;
 
 public class DummyBot {
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Сколько будет 2 + 2?".equals(question)) {
+            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+
+        return rsl;
+    }
+
 }
