22124d1307fd89e5bb8fe9ec89b9d7af287e0dbd
arkanomsk <49816097+arkanomsk@users.noreply.github.com>
2020-04-16 00:27:43 +0600

    5.5. Простое число.[#269255]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        int index;
+        for (index = 2; index <= number; number++) {
+            if ((number % index) == 0 ) {
+                prime = false;
+                break;
+            }
+            else if (number > index)
+                break;
+        }
+            return prime;
+        }
+
+    }
