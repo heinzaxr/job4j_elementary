cf5796ad5d338d283213fce7db148a8e46c448ae
arkanomsk <49816097+arkanomsk@users.noreply.github.com>
2020-04-23 00:36:47 +0600

    5.8. JavaDoc. Code convention [#269262]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,15 +1,33 @@
 package ru.job4j.converter;
+/*
+*Currency converter
+*
+*@author Alexandr Geynts (heinz-axr@ya.ru)
+*
+ */
 
 public class Converter {
+    /* Method rubleToEuro
+    *@param value of the Euro currency
+    *@return amount in roubles
+    */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
-
+    /* Method  rubleToDollar
+     *@param value of the Dollar currency
+     *@return amount in roubles
+     */
     public static int rubleToDollar(int value) {
         int rsl = value / 60;
         return rsl;
     }
+
+    /*
+    * Method Main
+    *@param args
+    */
 
     public static void main(String[] args) {
         int in = 140;
